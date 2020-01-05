3490638c576c68771b877ca9825a70fca500c9ef
Monick <monick.keo@gmail.com>
2020-01-04 19:15:47 -0800

    added page/quiz layout and conntected to books menu. no functionality yet

--- app/src/main/java/com/example/dokushoapp/MainActivity.java
+++ app/src/main/java/com/example/dokushoapp/MainActivity.java
@@ -9,16 +9,16 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button level1;
+    private Button selectedLevel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        level1 = findViewById(R.id.lvl1_button);
+        selectedLevel = findViewById(R.id.lvl1_button);
 
-        level1.setOnClickListener(new View.OnClickListener(){
+        selectedLevel.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, BooksMenu.class);
