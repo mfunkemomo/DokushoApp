e7521d40fc4fed2b16fded05adefb1a14d81e834
Monick <monick.keo@gmail.com>
2020-01-06 22:44:44 -0800

    reverted to using firestore, updated dependencies, still trying to get db to connect

--- app/src/main/java/com/example/dokushoapp/BooksMenu.java
+++ app/src/main/java/com/example/dokushoapp/BooksMenu.java
@@ -12,46 +12,48 @@ import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FieldValue;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.annotation.Nullable;
 
 public class BooksMenu extends AppCompatActivity {
 
-    private Button selectedBook;
+    private TextView bookSelection;
     private TextView booksMenuHeader;
     private static final String TAG = "BooksMenu";
 
     //Connection to Firebase
-//    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-//    private CollectionReference dbStories = db.collection("Stories");
+    private FirebaseFirestore db = FirebaseFirestore.getInstance();
+    private CollectionReference dbStories = db.collection("Stories");
 
-    FirebaseDatabase db = FirebaseDatabase.getInstance();
-    DatabaseReference dbStories = db.getReference("Stories");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_menu);
 
-        selectedBook = findViewById(R.id.book1_button);
         int level = getIntent().getIntExtra("level", 1);
+        bookSelection = findViewById(R.id.bookTitleAuthor);
 
         booksMenuHeader = findViewById(R.id.books_menu_header);
         booksMenuHeader.setText("Level " + String.valueOf(level) + " Books");
+        Log.d(TAG, "onCreate: able to retrieve level before getStories");
 
         getStories();
 
-        selectedBook.setOnClickListener(new View.OnClickListener(){
+        bookSelection.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(BooksMenu.this, StoryPage.class);
@@ -61,46 +63,31 @@ public class BooksMenu extends AppCompatActivity {
     }
 
     private void getStories(){
-        dbStories.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                // This method is called once with the initial value and again
-                // whenever data at this location is updated.
-                String value = dataSnapshot.getValue(String.class);
-                Log.d(TAG, "Value is: " + value);
-            }
 
-            @Override
-            public void onCancelled(DatabaseError error) {
-                // Failed to read value
-                Log.w(TAG, "Failed to read value.", error.toException());
-            }
-        });
+        dbStories.get()
+            .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+                @Override
+                public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+
+                    String data = "";
+                    for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
+                        Log.d(TAG, "onSuccess: " + snapshots.getId());
+
+                        Story story = snapshots.toObject(Story.class);
+
+                        data += story.getTitle() + " \n"
+                                + "By" + story.getAuthor() + "\n\n" ;
+
+                    }
+                    bookSelection.setText(data);
+
+                }
+            })
+            .addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d(TAG, "onError: " + e.toString());
+                }
+            });
     }
-//        dbStories.get()
-//                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
-//                    @Override
-//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
-//
-//                        String data = "";
-//                        for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
-//                            Log.d(TAG, "onSuccess: " + snapshots.getId());
-//
-//                            Story story = snapshots.toObject(Story.class);
-//
-//                            data += story.getTitle() + " \n"
-//                                    + "By" + story.getAuthor() + "\n\n" ;
-//
-//                        }
-//                        selectedBook.setText(data);
-//
-//                    }
-//                })
-//                .addOnFailureListener(new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        Log.d(TAG, "onError: " + e.toString());
-//                    }
-//                });
-//    }
 }
