30083f980cc6860c87500f959b2735eddf39b7df
Monick <monick.keo@gmail.com>
2020-01-06 16:15:38 -0800

    updated database to use realtime db

--- app/src/main/java/com/example/dokushoapp/BooksMenu.java
+++ app/src/main/java/com/example/dokushoapp/BooksMenu.java
@@ -1,5 +1,6 @@
 package com.example.dokushoapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -9,8 +10,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import org.w3c.dom.Text;
 
@@ -18,25 +29,27 @@ public class BooksMenu extends AppCompatActivity {
 
     private Button selectedBook;
     private TextView booksMenuHeader;
-
-    //get all the stories with the appropriate levels
-    // ex. stories_level1 = db.stories.level1
+    private static final String TAG = "BooksMenu";
 
     //Connection to Firebase
 //    private FirebaseFirestore db = FirebaseFirestore.getInstance();
-//    private DocumentReference dbStories = db.collection("Stories");
+//    private CollectionReference dbStories = db.collection("Stories");
+
+    FirebaseDatabase db = FirebaseDatabase.getInstance();
+    DatabaseReference dbStories = db.getReference("Stories");
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_menu);
 
+        selectedBook = findViewById(R.id.book1_button);
         int level = getIntent().getIntExtra("level", 1);
 
-        selectedBook = findViewById(R.id.book1_button);
         booksMenuHeader = findViewById(R.id.books_menu_header);
-
         booksMenuHeader.setText("Level " + String.valueOf(level) + " Books");
+
+        getStories();
 
         selectedBook.setOnClickListener(new View.OnClickListener(){
             @Override
@@ -46,4 +59,48 @@ public class BooksMenu extends AppCompatActivity {
             }
         });
     }
+
+    private void getStories(){
+        dbStories.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
+    }
+//        dbStories.get()
+//                .addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+//
+//                        String data = "";
+//                        for (QueryDocumentSnapshot snapshots : queryDocumentSnapshots) {
+//                            Log.d(TAG, "onSuccess: " + snapshots.getId());
+//
+//                            Story story = snapshots.toObject(Story.class);
+//
+//                            data += story.getTitle() + " \n"
+//                                    + "By" + story.getAuthor() + "\n\n" ;
+//
+//                        }
+//                        selectedBook.setText(data);
+//
+//                    }
+//                })
+//                .addOnFailureListener(new OnFailureListener() {
+//                    @Override
+//                    public void onFailure(@NonNull Exception e) {
+//                        Log.d(TAG, "onError: " + e.toString());
+//                    }
+//                });
+//    }
 }
---
+++ app/src/main/java/com/example/dokushoapp/Story.java
@@ -0,0 +1,17 @@
+package com.example.dokushoapp;
+
+public class Story {
+    private String title;
+    private String author;
+
+    //empty constructor for Firebase
+    public Story(){ }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+}
