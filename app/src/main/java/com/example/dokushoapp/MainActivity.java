dfda69fb30b41bfddfee79419350dbea7240400b
Monick <monick.keo@gmail.com>
2020-01-07 20:16:15 -0800

    added level2 button for book selection to mainactivity

--- app/src/main/java/com/example/dokushoapp/MainActivity.java
+++ app/src/main/java/com/example/dokushoapp/MainActivity.java
@@ -11,20 +11,31 @@ import com.google.firebase.firestore.FirebaseFirestore;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button selectedLevel;
+    private Button selectLevel1;
+    private Button selectLevel2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        selectedLevel = findViewById(R.id.lvl1_button);
+        selectLevel1 = findViewById(R.id.lvl1_button);
+        selectLevel2 = findViewById(R.id.lvl2_button);
 
-        selectedLevel.setOnClickListener(new View.OnClickListener(){
+        selectLevel1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 Intent intent = new Intent(MainActivity.this, BooksMenu.class);
                 intent.putExtra("level", 1);
+                startActivity(intent);
+            }
+        });
+
+        selectLevel2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                Intent intent = new Intent(MainActivity.this, BooksMenu.class);
+                intent.putExtra("level", 2);
                 startActivity(intent);
             }
         });
